7ba859fe494a6117034374144209aca53f6e882c
rhjje <numelen@gmail.com>
2020-09-28 22:36:42 +0300

    feat: done typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return '0';
+  }
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < result) {
+      result = array[i];
+    }
+  }
+
+  return result;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return '0';
+  }
+  let result = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > result) {
+      result = array[i];
+    }
+  }
+
+  return result;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return '0';
+  }
+  let avg = 0;
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+      avg += array[i];
+      count++;
+  }
+
+  return avg / count;
+};
+
